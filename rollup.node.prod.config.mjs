7433dfd2d5ae75fd7370f86161c4bce1bf002651
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2023-06-10 14:34:25 +0200

    fix build

--- rollup.node.prod.config.mjs
+++ rollup.node.prod.config.mjs
@@ -5,7 +5,7 @@ import commonMod from "./rollup.node.common.config.mjs"
 export default merge(commonMod, {
   input: 'app/src/colorfulCliLogger.ts',
   output: {
-    file: 'dist/cjs/colorfulCliLogger.js',
+    file: 'app/dist/cjs/colorfulCliLogger.js',
     format: 'cjs'
   },
 })
